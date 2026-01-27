4005c20715870a0adf6eda6f071a01a1b0d6b349
Caitlin R. R. Turner <75446150+caitlinturner@users.noreply.github.com>
2026-01-26 18:55:45 -0600

    Update downloader.py
    Signed-off-by: Caitlin R. R. Turner  <75446150+caitlinturner@users.noreply.github.com>

--- src/GaugePredict/downloader.py
+++ src/GaugePredict/downloader.py
@@ -32,6 +32,7 @@ def _rank_parameter_col(name, preference):
             return i
     return len(preference)
 
+
 def _pick_parameter_col(df, parameter_code, *, parameter_kind=None):
     """
     Select the preferred NWIS DV column corresponding to a parameter code.
